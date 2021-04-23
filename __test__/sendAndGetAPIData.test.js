3b2bf5b9face5ca8e3b006e043fea64e53eb4df7
Stefan Rausch <rausch.stefan@edumail.at>
2021-04-23 20:07:54 +0200

    add jest test

--- __test__/sendAndGetAPIData.test.js
+++ __test__/sendAndGetAPIData.test.js
@@ -4,6 +4,7 @@ import { sendAndGetAPIData } from "../src/client/js/apiRequestHandler"
 describe("Testing the apiRequestHandler functionality", () => {
     test("Testing the sendAndGetAPIData() function", () => {
         
+        // used this example test from Veselin T: https://knowledge.udacity.com/questions/507242
         document.body.innerHTML ="<button id='send-button'>Analyse</button>";
         const sendButton = document.getElementById("send-button");
         sendButton.click();
